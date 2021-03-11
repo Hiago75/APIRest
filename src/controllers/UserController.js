9b3b9f82e4cdd1bd31bf6342e8725d800444a99b
Hiagun1 <hiagobrenha75@gmail.com>
2021-03-11 01:57:12 -0300

    Terminado o CRUD do User e implantado sistema de JWT

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -5,9 +5,88 @@ class UserController {
     try {
       const novoUser = await User.create(req.body);
 
-      res.json(novoUser);
+      return res.json(novoUser);
     } catch (e) {
-      res.status(400).json({
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  /*
+  TODO
+  ** index **
+  ** show **
+  ** update **
+  delete
+  */
+
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id);
+      return res.json(user);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({
+          errors: ['ID não enviado'],
+        });
+      }
+
+      const user = await User.findByPk(id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Este usuário não existe'],
+        });
+      }
+
+      const newData = await user.update(req.body);
+
+      return res.json(newData);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({
+          errors: ['ID não enviado'],
+        });
+      }
+
+      const user = await User.findByPk(id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Este usuário não existe'],
+        });
+      }
+
+      await user.destroy(id);
+      return res.json(user);
+    } catch (e) {
+      return res.status(400).json({
         errors: e.errors.map((err) => err.message),
       });
     }
--- src/models/User.js
+++ src/models/User.js
@@ -49,8 +49,14 @@ export default class User extends Model {
     });
 
     this.addHook('beforeSave', async (user) => {
-      user.password_hash = await bcryptjs.hash(user.password, 10);
+      if (user.password) {
+        user.password_hash = await bcryptjs.hash(user.password, 10);
+      }
     });
     return this;
+  }
+
+  passwordIsValid(password) {
+    return bcryptjs.compare(password, this.password_hash);
   }
 }
